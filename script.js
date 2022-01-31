904c609f5a4c67c5e54b93615111b6766ba57766
Breno Bader <81591179+brenobader@users.noreply.github.com>
2022-01-30 22:23:54 -0300

    adiciona html e javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+//1 - Crie uma condição que exiba uma mensagem no console se a idade for maior que 18
+
+//2 - Crie uma condição que exiba uma mensagem no console se você for maior de idade E a variável "humano" seja true
+
+//3 - Criar uma condição que exiba uma mensagem no console se você fizer aniversário em Janeiro OU Dezembro
+let idade= 24;
+console.log(idade > 18?  'pode assistir corujão da globo, tenho mais de 18' : 'não pode assistir o corujão, não tem 18');
+
+let humano= true;
+console.log(idade > 18 && humano? 'tenho mais de 18 e sou humano' : 'não tem mais de 18 e não humano');
+
+let aniversário= maio 
+console.log(idade > 18 && humano && aniversário? 'não faço aniversário em dezembro ou janeiro' : 'faço aniversário em dezembro ou janeiro');
+
+
